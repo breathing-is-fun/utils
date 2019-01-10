f48cab09b7b0e9b2358362e865e4b1acd3518d9b
zy410419243 <zy410419243@gmail.com>
2019-01-10 14:19:59 +0800

    feat: add entry with lazy load

--- TransitionWithReactRouter/index.js
+++ TransitionWithReactRouter/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Suspense, lazy } from 'react';
 import { HashRouter as Router, Route } from 'react-router-dom';
 import { AnimationWrapper } from './AnimationWrapper';
 
@@ -10,7 +10,7 @@ import XcDetailIns from '../modules/xc-detail';
 const Xc = AnimationWrapper(XcIns);
 const XcDetail = AnimationWrapper(XcDetailIns);
 
-export default class Entry extends Component {
+class EntryWithoutLazy extends Component {
   render = () => {
     return (
       <Router>
@@ -24,6 +24,29 @@ export default class Entry extends Component {
             children={props => <XcDetail {...props} />}
           />
         </div>
+      </Router>
+    );
+  };
+}
+
+// 能分隔代码，但切换到 b 模块时，a 离场动画会加载，但 b 不会加载入场动画，只会加载 加载中...
+const Xc = AnimationWrapper(lazy(() => import('../modules/xc')));
+const XcDetail = AnimationWrapper(lazy(() => import('../modules/xc-detail')));
+
+class EntryWithLazy extends Component {
+  render = () => {
+    return (
+      <Router>
+        <Suspense fallback={<div>加载中...</div>}>
+          <Route
+            path="/a"
+            children={props => <Xc {...props} />}
+          />
+          <Route
+            path="/b"
+            children={props => <XcDetail {...props} />}
+          />
+        </Suspense>
       </Router>
     );
   };
